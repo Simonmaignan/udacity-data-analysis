55ae1f1d3d3784e7327ede0c5920289cdce2b103
Simon Maignan <simonmaignan88@gmail.com>
2020-12-06 19:11:49 +0100

    Clean function added to fill the NaN in a df Notebook updated to fill the NaN on each df

--- project-investigate-gapminder-dataset/data_cleaning.py
+++ project-investigate-gapminder-dataset/data_cleaning.py
@@ -5,13 +5,20 @@ import pandas as pd
 import numpy as np
 
 
-def fillNaN(dataframe):
-    print('test')
+def fillNaN(df):
+    # Start by propagating the next valid value to fill the NaNs 
+    # (this will fill all NaN except if there is just NaN until the end of the row)
+    df.fillna(method='bfill', axis='columns', inplace=True)
+    # Propagate the last valid value to fill the rest of the NaNs
+    df.fillna(method='ffill', axis='columns', inplace=True)
 
 
 def removeYearsInFuture(df, present_year=2018):
+    # Retrieve the highest year (last)
     max_year = df.columns.max()
+    # and create a vector of year to remove from DataFrame
     years_to_drop = np.arange(present_year + 1, max_year + 1)
+    
     df.drop(columns=years_to_drop, inplace=True)
 
 def columns2Ints(df):
