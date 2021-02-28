0e78d2175e8e41cb6d2731e7f07327ab146919cb
Simon Maignan <simonmaignan88@gmail.com>
2021-02-28 17:34:41 +0100

    First version of the data explanation

--- project-communicate-data-findings/loanDFWrangle.py
+++ project-communicate-data-findings/loanDFWrangle.py
@@ -3,10 +3,12 @@ import pandas as pd
 def categorizeVariables(df_loan_data):
     
     # Occupation from string to category
-    df_loan_data.Occupation = df_loan_data.Occupation.astype('category')
+    if 'Occupation' in df_loan_data.columns:
+        df_loan_data.Occupation = df_loan_data.Occupation.astype('category')
     
     # Loan status from string to ordered category
-    loan_status_list = ['Chargedoff',
+    if 'LoanStatus' in df_loan_data.columns:
+        loan_status_list = ['Chargedoff',
                         'Defaulted',
                         'Past Due (>120 days)',
                         'Past Due (91-120 days)',
@@ -17,11 +19,22 @@ def categorizeVariables(df_loan_data):
                         'Current',
                         'FinalPaymentInProgress',
                         'Completed',]
-    loan_status_class = pd.api.types.CategoricalDtype(ordered=True, categories=loan_status_list)
-    df_loan_data.LoanStatus = df_loan_data.LoanStatus.astype(loan_status_class);
+        loan_status_class = pd.api.types.CategoricalDtype(ordered=True, categories=loan_status_list)
+        df_loan_data.LoanStatus = df_loan_data.LoanStatus.astype(loan_status_class);
+        
+     # Loan status from string to ordered category
+    if 'LoanStatusSimplified' in df_loan_data.columns:
+        loan_simplified_status_list = ['PaymentsStopped',
+                                       'PastDue',
+                                       'Ongoing',
+                                       'Completed']
+        loan_simplified_status_class = pd.api.types.CategoricalDtype(ordered=True, categories=loan_simplified_status_list)
+        df_loan_data.LoanStatusSimplified = df_loan_data.LoanStatusSimplified.astype(loan_simplified_status_class);
     
     # Listing category from string to category
-    df_loan_data.ListingCategory = df_loan_data.ListingCategory.astype('category')
+    if 'ListingCategory' in df_loan_data.columns:
+        df_loan_data.ListingCategory = df_loan_data.ListingCategory.astype('category')
     
     # Loan origination date from string to DateTime
-    df_loan_data.LoanOriginationDate = pd.to_datetime(df_loan_data.LoanOriginationDate)
+    if 'LoanOriginationDate' in df_loan_data.columns:
+        df_loan_data.LoanOriginationDate = pd.to_datetime(df_loan_data.LoanOriginationDate)
